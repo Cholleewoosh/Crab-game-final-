ebfab8e116f2d7e2811ad50fe134bc3d9ffab0fe
Kailash Babu <kailashmjbabu@gmail.com>
2025-06-06 10:51:01 +1200

    I FIXED IT. Urchin enemy is working, that's basically all I did

--- Scenes/slime_3.gd
+++ Scenes/slime_3.gd
@@ -1,17 +1,14 @@
 extends CharacterBody2D
 
-var attack = true
+var spines_out = true
 
 @export var damage_amount : int = 1
 @onready var hurtbox: CollisionShape2D = $Hurtbox
 
-func _process(delta: float) -> void:
-	if attack == true:
-		hurtbox.disabled = true
-		await get_tree().create_timer(3).timeout
-		attack = false
-		
-	if attack == false:
+func _ready() -> void:
+	while spines_out == true:
 		hurtbox.disabled = false
-		await get_tree().create_timer(0.5).timeout
-		attack = true
+		await get_tree().create_timer(1.5).timeout
+		hurtbox.disabled = true
+		await get_tree().create_timer(1).timeout
+		
